4e420f4abc2630cd2cec26d1f127e745b0bc8439
Roland Rifandi Utama <roland_hawk@yahoo.com>
2017-05-25 11:56:55 +0700

    use current_login_at instead of updated_at

--- lib/authlogic/session/brute_force_protection.rb
+++ lib/authlogic/session/brute_force_protection.rb
@@ -58,7 +58,7 @@ module Authlogic
         # By default they will be banned for 2 hours. During that 2 hour period this method will return true.
         def being_brute_force_protected?
           exceeded_failed_logins_limit? && (failed_login_ban_for <= 0 ||
-            (attempted_record.respond_to?(:updated_at) && attempted_record.updated_at >= failed_login_ban_for.seconds.ago))
+            (attempted_record.respond_to?(:current_login_at) && attempted_record.current_login_at >= failed_login_ban_for.seconds.ago))
         end
         
         private
