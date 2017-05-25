8c1c962a27f79915d451f3223bb8ec3f3497f816
Roland Rifandi Utama <roland_hawk@yahoo.com>
2017-05-25 12:09:27 +0700

    nil check

--- lib/authlogic/session/brute_force_protection.rb
+++ lib/authlogic/session/brute_force_protection.rb
@@ -74,7 +74,9 @@ module Authlogic
         # that 2 hour period this method will return true.
         def being_brute_force_protected?
           exceeded_failed_logins_limit? && (failed_login_ban_for <= 0 ||
-            (attempted_record.respond_to?(:current_login_at) && attempted_record.current_login_at >= failed_login_ban_for.seconds.ago))
+            (attempted_record.respond_to?(:current_login_at) && 
+              attempted_record.current_login_at &&
+              attempted_record.current_login_at >= failed_login_ban_for.seconds.ago))
         end
 
         private
